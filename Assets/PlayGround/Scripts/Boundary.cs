545bc12f80cea0e8b15eb08beca8229e9585a48f
rkw <bartoszbrzozowskipl@gmail.com>
2019-05-02 17:45:19 +0200

    Check2

---
+++ Assets/PlayGround/Scripts/Boundary.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundary : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Cube")
+        {
+            Debug.Log(other.transform.position);
+            Destroy(other.gameObject);
+        }
+    }
+}
